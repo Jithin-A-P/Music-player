18359c2e4a69acadbe58cead16e45e957f5fbdfc
Jithin A P <jithinap009@gmail.com>
2020-07-31 12:57:58 +0530

    added fragments files, now its a mess, a lot of cleanup required

---
+++ app/src/main/java/com/music/player/adapters/ViewPagerAdapter.kt
@@ -0,0 +1,16 @@
+package com.music.player.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment){
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        TODO("Not yet implemented")
+
+    }
+
+}
---
+++ app/src/main/java/com/music/player/fragments/CollectionFragment.kt
@@ -0,0 +1,29 @@
+package com.music.player.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.music.player.R
+import com.music.player.adapters.ViewPagerAdapter
+
+class CollectionFragment : Fragment() {
+    private lateinit var viewPagerAdapter: ViewPagerAdapter
+    private lateinit var viewPager: ViewPager2
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_collection, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewPagerAdapter = ViewPagerAdapter(this)
+        viewPager = view.findViewById(R.id.pager)
+        viewPager.adapter = viewPagerAdapter
+    }
+}
